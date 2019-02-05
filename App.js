e26346109b3baf1e7f13e06e07d6d1b33a67ee38
nhaucorp <nazmul@springrainit.com>
2019-02-05 12:34:47 +0600

    init commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header,CryptoContainer } from './src/components';
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+
+export default class App extends React.Component {
+   render() {
+      return (
+         <Provider store={Store}>
+            <View>
+               <Header />
+               <CryptoContainer/>
+            </View>
+         </Provider>
+      );
+   }
+}
---
+++ src/components/CryptoContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Text } from 'react-native';
+
+class CryptoContainer extends Component {
+   render() {
+      return (
+         <View>
+            <Text>Container</Text>
+         </View>
+      );
+   }
+}
+
+function mapStateToProps(state) {
+   return {
+      crypto: state.crypro,
+   };
+}
+export default connect(mapStateToProps)(CryptoContainer);
